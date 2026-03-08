bc54819565240138d3e19a14044e4e32a794498f
Anwesha Dubey <anweshadubey13@gmail.com>
2026-03-09 02:08:21 +0530

    Refactor task rendering and adding functionality

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function renderTasks(){
+
 const list = document.getElementById("taskList");
 list.innerHTML="";
 
 tasks.forEach((task,index)=>{
 
 const li=document.createElement("li");
-li.textContent=task;
+
+const span=document.createElement("span");
+span.textContent=task.text;
+
+if(task.completed){
+span.classList.add("completed");
+}
+
+span.onclick=()=>{
+tasks[index].completed=!tasks[index].completed;
+saveTasks();
+renderTasks();
+};
 
 const delBtn=document.createElement("button");
-delBtn.textContent="Delete";
+delBtn.textContent="X";
+delBtn.className="delete-btn";
 
 delBtn.onclick=()=>{
 tasks.splice(index,1);
@@ -18,20 +32,29 @@ saveTasks();
 renderTasks();
 };
 
+li.appendChild(span);
 li.appendChild(delBtn);
+
 list.appendChild(li);
 
 });
+
+document.getElementById("taskCount").innerText = tasks.length + " Tasks";
+
 }
 
 function addTask(){
 
 const input=document.getElementById("taskInput");
-const task=input.value.trim();
 
-if(task==="") return;
+const text=input.value.trim();
 
-tasks.push(task);
+if(text==="") return;
+
+tasks.push({
+text:text,
+completed:false
+});
 
 saveTasks();
 renderTasks();
@@ -42,5 +65,12 @@ input.value="";
 function saveTasks(){
 localStorage.setItem("tasks",JSON.stringify(tasks));
 }
+
+document.getElementById("taskInput")
+.addEventListener("keypress",function(e){
+if(e.key==="Enter"){
+addTask();
+}
+});
 
 renderTasks();
